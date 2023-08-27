8d3dca894d3cdc252cb2b374c0c159d370b03839
Isaschar Madar <pryysy88@gmail.com>
2023-08-27 10:46:02 +0300

    add Square exercise

--- index.ts
+++ index.ts
@@ -1,22 +1,30 @@
 // את הקוד שלכם תכתבו כאן
-class Rectangle{
+
+//  1. Rectangle - מלבן
+class Rectangle {
     hight: number;
     width: number;
-
-
-constructor(hight: number, width: number){
-    this.hight = hight
-    this.width = width
-}
-
-area():number{
-    return this.hight * this.width
-}
-}
-//  1. Rectangle - מלבן
+  
+    constructor(hight: number, width: number) {
+      this.hight = hight;
+      this.width = width;
+    }
+  
+    area(): number {
+      return this.hight * this.width;
+    }
+  }
 
 //  2. Square - ריבוע
-
+class Square extends Rectangle {
+    ribHight: number;
+  
+    constructor(hight: number, width: number, ribHight: number) {
+      super(hight, width);
+      this.ribHight = ribHight;
+    }
+  }
+  
 // 3. Shape - צורה
 
 // 4. Method Chaining
