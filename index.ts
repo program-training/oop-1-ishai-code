d927894f01920a5fb5cc0eaddfc345b5628bc45d
Isaschar Madar <pryysy88@gmail.com>
2023-08-27 10:39:06 +0300

    reactangle exercise

--- index.ts
+++ index.ts
@@ -1,5 +1,18 @@
 // את הקוד שלכם תכתבו כאן
+class Rectangle{
+    hight: number;
+    width: number;
 
+
+constructor(hight: number, width: number){
+    this.hight = hight
+    this.width = width
+}
+
+area():number{
+    return this.hight * this.width
+}
+}
 //  1. Rectangle - מלבן
 
 //  2. Square - ריבוע
